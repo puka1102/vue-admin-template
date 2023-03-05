e0c27110408f77fff549453a78cff8295b0ec30a
puka1102 <yuzurupkq@gmail.com>
2023-03-05 21:53:57 +0800

    1.多页签的导入

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,7 @@ import Print from 'vue-print-nb'
 import ScreenFull from './screenfull'
 import ThemePicker from './ThemePicker'
 import Lang from './lang'
+import TagsView from './TagsView'
 export default {
   install(Vue) {
     // 注册全局的通用栏组件对象
@@ -14,6 +15,7 @@ export default {
     Vue.component('ScreenFull', ScreenFull)
     Vue.component('ThemePicker', ThemePicker)
     Vue.component('Lang', Lang)
+    Vue.component('TagsView', TagsView)
     Vue.use(Print) // 注册打印组件
   }
 }
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -5,6 +5,8 @@
     <div class="main-container">
       <div :class="{'fixed-header':fixedHeader}">
         <navbar />
+        <!-- 放置多页标签 -->
+        <tags-view />
       </div>
       <app-main />
     </div>
